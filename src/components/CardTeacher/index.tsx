3b920c5d2f322272378c3f9a2d2d8240eddd8d98
pedropsoares <pedropaulos.1998@gmail.com>
2020-08-04 16:54:31 -0300

    cardTeacher created and stylized

---
+++ src/components/CardTeacher/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+
+import './style.css';
+
+const CardTeacher: React.FC = () => {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img src="https://avatars2.githubusercontent.com/u/39010327?s=460&v=4" alt="Pedro Soares" />
+        <div>
+          <strong>Pedro Soares</strong>
+          <span>Quimica</span>
+        </div>
+      </header>
+
+      <p>
+        Entusiasta das melhores tecnologias de química avançada.
+        <br /> <br />
+        Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
+      </p>
+
+      <footer>
+        <p>
+          Preço/hora
+          <strong>R$: 80,00</strong>
+        </p>
+        <button type="button">
+          <img src={whatsappIcon} alt="whatsapp" />
+              Entrar em contato
+            </button>
+      </footer>
+    </article>
+  )
+}
+
+export default CardTeacher;
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import PageHearder from '../../components/PagaHeader'
 
 import './style.css';
+import CardTeacher from '../../components/CardTeacher';
 
 const TeacherForm: React.FC = () => {
   return (
@@ -11,21 +12,25 @@ const TeacherForm: React.FC = () => {
         <form id="search-teachers">
           <div className="input-block">
             <label htmlFor="subject">Matérias</label>
-            <input type="text" id="subject"/>
+            <input type="text" id="subject" />
           </div>
-          
+
           <div className="input-block">
             <label htmlFor="week_day">Dia da semana</label>
-            <input type="text" id="week_day"/>
+            <input type="text" id="week_day" />
           </div>
 
           <div className="input-block">
             <label htmlFor="time">Hora</label>
-            <input type="text" id="time"/>
+            <input type="text" id="time" />
           </div>
 
         </form>
       </PageHearder>
+
+      <main>
+        <CardTeacher />
+      </main>
     </div>
   )
 }
