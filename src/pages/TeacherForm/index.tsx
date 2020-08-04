800b87b140796b89a0366b1f8a352689ace9782b
pedropsoares <pedropaulos.1998@gmail.com>
2020-08-04 14:48:43 -0300

    component PageHeader created

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import PageHearder from '../../components/PagaHeader'
+
 const TeacherForm: React.FC = () => {
-  return <div />;
+  return (
+    <div id="page-teacher-form" className="container">
+      <PageHearder
+        title="Que incrível que você quer dar aulas."
+      />
+    </div>
+  )
 }
 
 export default TeacherForm;
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,28 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-import logoImg from '../../assets/images/logo.svg'
-import backIcon from '../../assets/images/icons/back.svg'
+import PageHearder from '../../components/PagaHeader'
 
 import './style.css';
 
 const TeacherForm: React.FC = () => {
   return (
     <div id="page-teacher-list" className="container">
-      <header className="page-header">
-        <div className="top-bar-container">
-          <Link to="/">
-            <img src={backIcon} alt="voltar" />
-          </Link>
-          <img src={logoImg} alt="proffy" />
-        </div>
-
-        <div className="header-content">
-          <strong>
-            Estes são os proffys disponiveis.
-          </strong>
-        </div>
-      </header>
+      <PageHearder 
+        title="Estes são os
+        proffys disponíveis."
+      />
     </div>
   )
 }
