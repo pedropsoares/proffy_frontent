15741381b82a242847d7a09da28a9e6633e94868
pedropsoares <pedropaulos.1998@gmail.com>
2020-08-09 18:05:51 -0300

    *

--- src/components/CardTeacher/index.tsx
+++ src/components/CardTeacher/index.tsx
@@ -4,30 +4,38 @@ import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './style.css';
 
-const CardTeacher: React.FC = () => {
+export interface Teacher {
+  id: number;
+  name: string;
+  avatar: string;
+  bio: string;
+  cost: number;
+  subject: string;
+  whatsapp: string;
+}
+interface TeacherItemProps {
+  teacher: Teacher
+}
+
+const CardTeacher: React.FC<TeacherItemProps> = ({ teacher }) => {
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://avatars2.githubusercontent.com/u/39010327?s=460&v=4" alt="Pedro Soares" />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Pedro Soares</strong>
-          <span>Quimica</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
       <p>
-        Entusiasta das melhores tecnologias de química avançada.
-        <br />
-        <br />
-        Apaixonado por explodir coisas em laboratório e por mudar a vida
-        das pessoas através de experiências. Mais de 200.000 pessoas já
-        passaram por uma das minhas explosões.
+        {teacher.bio}
       </p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$: 80,00</strong>
+          <strong>R$: {teacher.cost}</strong>
         </p>
         <button type="button">
           <img src={whatsappIcon} alt="whatsapp" />
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,20 +1,41 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 
 import PageHearder from '../../components/PagaHeader';
-import CardTeacher from '../../components/CardTeacher';
+import CardTeacher, { Teacher } from '../../components/CardTeacher';
 import Input from '../../components/Input';
 import Select from '../../components/Select/index';
+import api from '../../services/api';
 
 import './style.css';
 
 const TeacherForm: React.FC = () => {
+  const [teachers, setTeachers] = useState([]);
+
+  const [subject, setSubject] = useState('');
+  const [week_day, setWeek_day] = useState('');
+  const [time, setTime] = useState('');
+
+  const seacherTeachers = async (e: FormEvent) => {
+    e.preventDefault();
+
+    const response = await api.get('classes', {
+      params: {
+        subject,
+        week_day,
+        time,
+      },
+    });
+    setTeachers(response.data);
+  };
   return (
     <div id="page-teacher-list" className="container">
       <PageHearder title="Estes são os proffys disponíveis.">
-        <form id="search-teachers">
+        <form id="search-teachers" onSubmit={seacherTeachers}>
           <Select
             name="subject"
             label="Matéria"
+            value={subject}
+            onChange={(e) => { setSubject(e.target.value); }}
             options={[
               { value: 'Artes', label: 'Artes' },
               { value: 'Biologia', label: 'Biologia' },
@@ -27,6 +48,8 @@ const TeacherForm: React.FC = () => {
           <Select
             name="week_day"
             label="Dia da semana"
+            value={week_day}
+            onChange={(e) => { setWeek_day(e.target.value); }}
             options={[
               { value: '0', label: 'Domingo' },
               { value: '1', label: 'Segunda' },
@@ -37,13 +60,24 @@ const TeacherForm: React.FC = () => {
               { value: '6', label: 'Sabado' },
             ]}
           />
-          <Input type="time" name="time" label="Hora" />
+          <Input
+            type="time"
+            name="time"
+            label="Hora"
+            value={time}
+            onChange={(e) => { setTime(e.target.value); }}
+          />
 
+            <button type="submit">
+              Buscar
+            </button>
         </form>
       </PageHearder>
 
       <main>
-        <CardTeacher />
+        {teachers.map((teacher: Teacher) => {
+          return <CardTeacher key={teacher.id} teacher={teacher} />;
+        })}
       </main>
     </div>
   );
